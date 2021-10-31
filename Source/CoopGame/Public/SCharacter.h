0257fd8e63bd22bee5206eaf03fbb9ec02ae6732
Palms <franco.rvl91@gmail.com>
2021-10-31 00:47:09 -0300

    Add Movement Input

---
+++ Source/CoopGame/Public/SCharacter.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
+#include "SCharacter.generated.h"
+
+class UCameraComponent;
+
+UCLASS()
+class COOPGAME_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(float AxisValue);	
+	void MoveRight(float AxisValue);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Components")
+	UCameraComponent* CameraComp;
+	
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
