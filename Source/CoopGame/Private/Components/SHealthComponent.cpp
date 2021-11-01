bf8f69b9f55763b84a729a79492416f1ae92c905
Palms <franco.rvl91@gmail.com>
2021-11-01 20:30:46 -0300

    Health Component

---
+++ Source/CoopGame/Private/Components/SHealthComponent.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/SHealthComponent.h"
+
+// Sets default values for this component's properties
+USHealthComponent::USHealthComponent()
+{
+
+	Health = 100.0f;
+}
+
+
+// Called when the game starts
+void USHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
