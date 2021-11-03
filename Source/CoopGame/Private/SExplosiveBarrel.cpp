f40ae6f8ed60ad2b205e7e669136387205d347ea
Palms <franco.rvl91@gmail.com>
2021-11-02 23:28:58 -0300

    Explosive Barrel Replication

--- Source/CoopGame/Private/SExplosiveBarrel.cpp
+++ Source/CoopGame/Private/SExplosiveBarrel.cpp
@@ -4,6 +4,7 @@
 #include "SExplosiveBarrel.h"
 
 #include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
 
 // Sets default values
 ASExplosiveBarrel::ASExplosiveBarrel()
@@ -25,6 +26,9 @@ ASExplosiveBarrel::ASExplosiveBarrel()
 	RadialForceComp->bIgnoreOwningActor = true; // Ignore self
 
 	ExplosionImpulse = 400;
+
+	SetReplicates(true);
+	AActor::SetReplicateMovement(true);
 }
 
 void ASExplosiveBarrel::OnHealthChanged(USHealthComponent* OwningHealthComp, float Health, float HealthDelta,
@@ -40,15 +44,11 @@ void ASExplosiveBarrel::OnHealthChanged(USHealthComponent* OwningHealthComp, flo
 	{
 		// Explode!
 		bExploded = true;
+		OnRep_Exploded();
 
 		// Boost the barrel upwards
 		FVector BoostIntensity = FVector::UpVector * ExplosionImpulse;
 		MeshComp->AddImpulse(BoostIntensity, NAME_None, true);
-
-		// Play FX and change self material to black
-		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
-		// Override material on mesh with blackened version
-		MeshComp->SetMaterial(0, ExplodedMaterial);
 
 		// Blast away nearby physics actors
 		RadialForceComp->FireImpulse();
@@ -57,5 +57,23 @@ void ASExplosiveBarrel::OnHealthChanged(USHealthComponent* OwningHealthComp, flo
 		
 	}
 	
+}
+
+void ASExplosiveBarrel::OnRep_Exploded() const
+{
+	// Play FX and change self material to black
+	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
+	// Override material on mesh with blackened version
+	MeshComp->SetMaterial(0, ExplodedMaterial);
+}
+
+
+void ASExplosiveBarrel::GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(ASExplosiveBarrel, bExploded);
+
+	//DOREPLIFETIME_CONDITION(ASExplosiveBarrel, bExploded, COND_OwnerOnly);
 }
 
