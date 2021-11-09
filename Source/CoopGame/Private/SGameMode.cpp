cd658d441c94c2f728e61d80eb8ce7c0aa3e79d6
Palms <franco.rvl91@gmail.com>
2021-11-09 02:01:24 -0300

    Wave Based Spawing

--- Source/CoopGame/Private/SGameMode.cpp
+++ Source/CoopGame/Private/SGameMode.cpp
@@ -2,4 +2,93 @@
 
 
 #include "SGameMode.h"
+#include "EngineUtils.h"
+#include "Components/SHealthComponent.h"
 
+
+ASGameMode::ASGameMode()
+{
+	TimeBetweenWaves = 2.0f;
+
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.TickInterval = 1.0f;
+}
+
+void ASGameMode::StartWave()
+{
+	WaveCount++;
+	
+	NroOfBotsToSpawn = 2 * WaveCount;
+	
+	GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ASGameMode::SpawnBotTimerElapsed, 1.0F, true, 0.0f);
+}
+
+void ASGameMode::EndWave()
+{
+	GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+}
+
+void ASGameMode::PrepareForNextWave()
+{
+	
+	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
+}
+
+void ASGameMode::CheckWaveState()
+{
+	bool bIPreparingForWave = GetWorldTimerManager().IsTimerActive(TimerHandle_NextWaveStart);
+	
+	if (NroOfBotsToSpawn > 0 || bIPreparingForWave)
+	{
+		return;
+	}
+	
+	bool bIsAntBotAlive = false;
+
+	for (TActorIterator<APawn> It(GetWorld()); It; ++It)
+	{
+		APawn *TestPawn = *It;
+		if (TestPawn == nullptr || TestPawn->IsPlayerControlled())
+		{
+			continue;
+		}
+
+		USHealthComponent* HealthComp = Cast<USHealthComponent>(TestPawn->GetComponentByClass(USHealthComponent::StaticClass()));
+		if (HealthComp && HealthComp->GetHealth() > 0.0F)
+		{
+			bIsAntBotAlive = true;
+			break;
+		}
+	}
+
+	if (!bIsAntBotAlive)
+	{
+		PrepareForNextWave();
+	}
+}
+
+void ASGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	PrepareForNextWave();
+}
+
+void ASGameMode::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	CheckWaveState();
+}
+
+void ASGameMode::SpawnBotTimerElapsed()
+{
+	SpawnNewBot();
+
+	NroOfBotsToSpawn--;
+
+	if (NroOfBotsToSpawn <= 0)
+	{
+		EndWave();
+	}
+}
--- Source/CoopGame/Public/SGameMode.h
+++ Source/CoopGame/Public/SGameMode.h
@@ -13,5 +13,43 @@ UCLASS()
 class COOPGAME_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+protected:
 	
+	FTimerHandle TimerHandle_BotSpawner;
+
+	FTimerHandle TimerHandle_NextWaveStart;
+
+	// Bots to spawn in a current wave
+	int32 NroOfBotsToSpawn;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category= "GameMode")
+	float TimeBetweenWaves;
+
+	// Hook for BP to spawn a single bot
+	UFUNCTION(BlueprintImplementableEvent, Category= "GameMode")
+	void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
+
+	// Start spawning bots
+	void StartWave();
+
+	// Stop spawning bots
+	void EndWave();
+
+	// Set timer for next startwave
+	void PrepareForNextWave();
+
+	void CheckWaveState();
+
+public:
+
+	ASGameMode();
+
+	virtual void StartPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;	
 };
